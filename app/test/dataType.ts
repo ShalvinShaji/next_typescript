95ccfcf87194e5f7ae4c2b01496f94fe262538aa
Shalvin <shalvinshaji283@gmail.com>
2024-10-31 15:42:52 +0530

    typescript basics

---
+++ app/test/dataType.ts
@@ -0,0 +1,12 @@
+let first_name:string = 'Shalvin'
+let age:number = 24
+let married:boolean = false
+let trip:any = false
+
+trip = 'Not going'
+
+
+console.log(first_name)
+console.log(age)
+console.log(married)
+console.log(trip)
